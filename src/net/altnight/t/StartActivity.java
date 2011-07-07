0c9459b518c84e75fecdb9bed32efcb839be664c
atasatamatara <atasatamatara@gmail.com>
2011-07-07 16:22:17 +0900

    spell miss fixed

--- src/net/altnight/t/StartActivity.java
+++ src/net/altnight/t/StartActivity.java
@@ -8,31 +8,31 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class StartActivity extends Activity {
-    /** Called when the activity is first created. */
-	
-	//twitter api key
+	/** Called when the activity is first created. */
+
+	// twitter api key
 	private String CONSUMER_KEY = "";
 	private String CONSUMER_SECRET = "";
-	
+
 	private AccessToken accessToken = null;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        try {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		try {
 			tweet();
 		} catch (TwitterException e) {
 			e.printStackTrace();
 		}
 		finish();
-    }
-    
-    public void tweet() throws TwitterException{
-    	//tweet‚·‚é
-    	Twitter twitter = new TwitterFactory().getInstance();
-    	twitter.updateStatus("testhogera------");
-    	
-    }
+	}
+
+	public void tweet() throws TwitterException {
+		// tweeting
+		Twitter twitter = new TwitterFactory().getInstance();
+		twitter.updateStatus("testhogera------");
+
+	}
 }
