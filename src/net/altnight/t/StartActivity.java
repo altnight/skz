e399e92eefb8310db09920768e56105cf1fdc797
atasatamatara <atasatamatara@gmail.com>
2011-07-11 17:37:26 +0900

    tweeting success

--- src/net/altnight/t/StartActivity.java
+++ src/net/altnight/t/StartActivity.java
@@ -4,62 +4,77 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 
 public class StartActivity extends Activity {
 	/** Called when the activity is first created. */
 
 	// twitter api key
-	private String CONSUMER_KEY = "";
-	private String CONSUMER_SECRET = "";
+//	private String CONSUMER_KEY = "";
+//	private String CONSUMER_SECRET = "";
 
-	private AccessToken accessToken = null;
+//	private Twitter twitter = new TwitterFactory().getInstance();
 
-	private boolean CONNECTED = false;
-	
+//	private AccessToken accessToken = null;
+//	private RequestToken reqestToken = null;
+//	private boolean CONNECTED = false;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		if (isConnected()) {
-			try {
-				tweet();
-			} catch (TwitterException e) {
-				e.printStackTrace();
-			}
-			finish();
-
-		} else {
-			try {
-				connectOAuth();
-			} catch (TwitterException e) {
-				e.printStackTrace();
-			}
+		// if (isConnected(CONNECTED)) {
+		try {
+			tweet();
+		} catch (TwitterException e) {
+			e.printStackTrace();
 		}
+		finish();
+
+		// } else {
+		// try {
+		// connectOAuth();
+		// } catch (TwitterException e) {
+		// e.printStackTrace();
+		// }
+		// }
 	}
 
-	private void connectOAuth() throws TwitterException {
-		Twitter twitter = new TwitterFactory().getInstance();
-		twitter.getOAuthRequestToken();
-		twitter.getAuthorization();
-	}
-	private boolean isConnected() {
-		if (CONNECTED){
-			CONNECTED = true;
-			return true;
-		} else{
-			CONNECTED = false;
-			return false;
-		}
-	}
+//	private void connectOAuth() throws TwitterException {
+//		SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+//		String oatuhToken = pref.getString("oauth_token", "");
+//		String oauthTokenSecret = pref.getString("oauth_token_secret", "");
+//		Twitter twitter = new TwitterFactory().getInstance();
+//		twitter.getOAuthRequestToken();
+//		twitter.getAuthorization();
+//	}
+
+//	private boolean isConnected(boolean CONNECTED) {
+//		if (CONNECTED) {
+//			CONNECTED = true;
+//			return true;
+//		} else {
+//			CONNECTED = false;
+//			return false;
+//		}
+//	}
 
 	public void tweet() throws TwitterException {
 		// tweeting
-		Twitter twitter = new TwitterFactory().getInstance();
-		twitter.updateStatus("testhogera------");
+		String consumerKey = "87K4CPLxFbe1v70fGpmhg";
+		String consumerSecret = "K6cOhBV4jWARRftJSHpTEuc2fBf3bvWJZOPziMYHM";
+		String accessToken = "3911141-eRXbAyzfmBp10ROJAVNqHcrhqjOKDrhGCGLung36R8";
+		String accessTokenSecret = "PzIqpTc4nwAfyDhKjIeQmOdFgBBzRYcEpzptr7jXAc";
+		
+		TwitterFactory factory = new TwitterFactory();
+		Twitter twitter = factory.getInstance();
+		twitter.setOAuthConsumer(consumerKey,consumerSecret);
+		twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+		twitter.updateStatus("日本語でおk");
 
 	}
 }
