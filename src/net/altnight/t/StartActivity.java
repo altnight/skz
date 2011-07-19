f5b1f9878ef9f1c2d169a91f496affe6810af3ce
atasatamatara <atasatamatara@gmail.com>
2011-07-19 17:15:13 +0900

    try mock

--- src/net/altnight/t/StartActivity.java
+++ src/net/altnight/t/StartActivity.java
@@ -5,6 +5,7 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,75 +14,85 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class StartActivity extends Activity {
-	/** Called when the activity is first created. */
+	// put your keys
+	final String consumerKey = "";
+	final String consumerSecret = "";
+	final String accessToken = "";
+	final String accessTokenSecret = "";
 
-	// private boolean CONNECTED = false;
+	private static final boolean CONNECTED = false;
+
+	/** Called when the activity is first created. */
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		Button tweetButton = (Button) findViewById(R.id.sendButton);
-		tweetButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				sendButton_OnClick();
+		Button tweet = (Button) findViewById(R.id.sendButton);
+		tweet.setOnClickListener(new TweetButton());
+
+		if (isConnected(CONNECTED)) {
+
+		} else {
+			try {
+				connectOAuth();
+			} catch (TwitterException e) {
+				e.printStackTrace();
 			}
-		});
+		}
 
-		// if (isConnected(CONNECTED)) {
-
-		// } else {
-		// try {
-		// connectOAuth();
-		// } catch (TwitterException e) {
-		// e.printStackTrace();
-		// }
-		// }
 	}
 
-	// private void connectOAuth() throws TwitterException {
-	// SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
-	// String oatuhToken = pref.getString("oauth_token", "");
-	// String oauthTokenSecret = pref.getString("oauth_token_secret", "");
-	// Twitter twitter = new TwitterFactory().getInstance();
-	// twitter.getOAuthRequestToken();
-	// twitter.getAuthorization();
-	// }
+	private void connectOAuth() throws TwitterException {
+		SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+		String oatuhToken = pref.getString("oauth_token", "");
+		String oauthTokenSecret = pref.getString("oauth_token_secret", "");
+		Twitter twitter = new TwitterFactory().getInstance();
+		twitter.getOAuthRequestToken();
+		twitter.getAuthorization();
+	}
 
-	// private boolean isConnected(boolean CONNECTED) {
-	// if (CONNECTED) {
-	// CONNECTED = true;
-	// return true;
-	// } else {
-	// CONNECTED = false;
-	// return false;
-	// }
-	// }
-	private void sendButton_OnClick() {
-		try {
-			tweet();
-		} catch (TwitterException e) {
-			e.printStackTrace();
+	private boolean isConnected(boolean CONNECTED) {
+		if (CONNECTED) {
+			CONNECTED = true;
+			return true;
+		} else {
+			CONNECTED = false;
+			return false;
 		}
-//		finish();
+	}
+
+	public class TweetButton implements OnClickListener {
+		@Override
+		public void onClick(View arg0) {
+			Toast.makeText(StartActivity.this, "送信したい……", Toast.LENGTH_SHORT)
+					.show();
+			try {
+				tweet();
+			} catch (TwitterException e) {
+				e.printStackTrace();
+			}
+
+		}
+		// finish();
 
 	}
 
 	public void tweet() throws TwitterException {
-		// tweeting
-		final String consumerKey = "";
-		final String consumerSecret = "";
-		final String accessToken = "";
-		final String accessTokenSecret = "";
 
 		EditText tweetText = (EditText) findViewById(R.id.tweetText);
-		
+		// tweetText.setText("defalut");
+		tweetText.setHint("hit-a-hint");
+		tweetText.setMaxLines(2);
+
 		Twitter twitter = new TwitterFactory().getInstance();
 		twitter.setOAuthConsumer(consumerKey, consumerSecret);
-		twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+		twitter.setOAuthAccessToken(new AccessToken(accessToken,
+				accessTokenSecret));
+
 		twitter.updateStatus(tweetText.getText().toString());
+		Toast.makeText(this, "送信したかもしれないです！", Toast.LENGTH_LONG).show();
 
 	}
 }
---
+++ src/net/altnight/t/TwitterLogin.java
@@ -0,0 +1,15 @@
+package net.altnight.t;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TwitterLogin extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.twitterlogin);
+		
+	}
+
+}
